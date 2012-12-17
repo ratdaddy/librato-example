82eed2fc163cd693421c5b7e85df9cbe29e17927
Brian VanLoo <brian.vanloo@gmail.com>
2012-12-16 23:06:42 -0800

    Initial commit

---
+++ app/controllers/ad_tracker_controller.rb
@@ -0,0 +1,31 @@
+require 'librato/metrics'
+
+class AdTrackerController < ApplicationController
+  def start
+    puts '$$$ got an ad start'
+    ad_counter.increment :fired, params[:provider]
+    render nothing: true
+  end
+
+  def status
+    puts '$$$ got ad status'
+    ad_counter.increment :status, "#{params[:provider]}-#{params[:result]}"
+    render nothing: true
+  end
+
+  def ad_counter
+    AddCounter
+  end
+end
+
+class AddCounter
+  Librato::Metrics.authenticate 'brian-com@thevanloos.com', ENV['LIBRETO_KEY']
+
+  def self.aggregator
+    @@aggregator ||= Librato::Metrics::Aggregator.new source: :test_app1, autosubmit_interval: 30
+  end
+
+  def self.increment event, provider
+    self.aggregator.add "#{event}-#{provider}" => 1
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Librato::Application.config.secret_token = '17cbeeef060892b16344b6788bc1b27e38b3b8cbcad65e6e929f1266f9f7635c79e4132433fbfc088149f45882dd2ed70455237e54db1d5ee059302e6b7ff630'
